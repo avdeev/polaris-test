63c180732846b8f0171eecd43b021dc23a32078a
Alexey Avdeev <aad.jerry@gmail.com>
2019-02-21 13:25:40 +0300

    Updates

---
+++ precache-manifest.eb9c0c3e7054014730fa5d20b3a6050e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4318cf2f6452bff2f19e",
+    "url": "/static/js/main.4318cf2f.chunk.js"
+  },
+  {
+    "revision": "907f38f1f8a2e076859f",
+    "url": "/static/js/1.907f38f1.chunk.js"
+  },
+  {
+    "revision": "907f38f1f8a2e076859f",
+    "url": "/static/css/1.ee85b616.chunk.css"
+  },
+  {
+    "revision": "75f1c836d602234059cdbf4c8350e58d",
+    "url": "/index.html"
+  }
+];
